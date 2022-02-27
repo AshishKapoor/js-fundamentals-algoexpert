44c895fbdaf8829726a8f6ce7853c1ea37a9fc89
Ashish Kapoor <swiftobjc@gmail.com>
2022-02-28 00:12:11 +0530

    Create promisify.js

---
+++ promisify.js
@@ -0,0 +1,17 @@
+// Promise internals
+function promisify(callback) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function handleErrorAndValue(error, value) {
+        if (error == null) {
+          resolve(value);
+        } else {
+          reject(error);
+        }
+      }
+      callback.call(this, ...args, handleErrorAndValue);
+    });
+  }
+}
+
+exports.promisify = promisify;
