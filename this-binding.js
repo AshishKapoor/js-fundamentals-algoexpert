9123adbc3692cf951fa7a749a1fec99f183ce378
Ashish Kapoor <swiftobjc@gmail.com>
2022-02-28 00:19:39 +0530

    Update this-binding.js

--- this-binding.js
+++ this-binding.js
@@ -1,3 +1,7 @@
+// Call invokes the function and allows you to pass in arguments one by one.
+// Apply invokes the function and allows you to pass in arguments as an array.
+// Bind returns a new function, allowing you to pass in a this array and any number of arguments.
+
 Function.prototype.myCall = function(thisContext, ...args) {
   const symbol = Symbol();
   thisContext[symbol] = this;
